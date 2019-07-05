ff540fb3f5844c798a8a3fcb6972a244116991e0
Michel <michel@mails.ch>
2019-07-05 11:30:51 +0200

    resolve filename for dirname

--- server.mjs
+++ server.mjs
@@ -2,6 +2,7 @@ import Datastore from 'nedb-promise';
 import bodyParser from 'body-parser';
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { TaskStore } from './services/taskStore.mjs';
 import { TaskController } from './controller/taskController.mjs';
 import { TaskRoutes } from './routes/taskRoutes.mjs';
@@ -9,7 +10,9 @@ import { TaskRoutes } from './routes/taskRoutes.mjs';
 const database = new Datastore({ filename: './data/tasks.db', autoload: true });
 const taskStore = new TaskStore(database);
 const taskController = new TaskController(taskStore);
-const __dirname = path.resolve(path.dirname(decodeURI(new URL(import.meta.url).pathname)));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.port || 8080;
